880c6e01b25f52ec55be60704dc351f0847807a1
benceszarka <szarkabence@gmail.com>
2023-03-22 11:12:14 +0100

    Added additional columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,12 @@ streamlit.text('Below you can see your previously added information:')
 
 def get_user_data():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select username, origin, destination, max_price from USER_PREFERENCES;")
+        my_cur.execute("select username, origin, destination, max_price, period, tier, email, email_noti, phone, sms_noti from USER_PREFERENCES;")
         return my_cur.fetchall()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_data_rows = get_user_data()
 my_cnx.close()
 display_data = pandas.DataFrame(my_data_rows)
-display_data.columns = ['Username', 'Origin', 'Destination', 'Budget']
+display_data.columns = ['Username', 'Origin', 'Destination', 'Budget', 'Period', 'Tier', 'E-mail', 'E-mail notification', 'Phone', 'SMS notification']
 streamlit.dataframe(display_data)
