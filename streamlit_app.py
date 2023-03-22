5011c8810f199256e8e5930b56b1443a89671766
benceszarka <szarkabence@gmail.com>
2023-03-22 10:43:10 +0100

    Snowflake connection implemented
    Added snowflake connection, with a simple query from snowflake being displayed.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,21 @@
 
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('Flight Hunter')
+
+streamlit.header('Welcome to the Flight Hunter app!')
+streamlit.text('Below you can see your previously added information:')
+
+def get_user_data():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select username, origin, destination, max_price from USER_PREFERENCES;")
+        return my_cur.fetchall()
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows = get_user_data()
+my_cnx.close()
+streamlit.dataframe(my_data_rows)
