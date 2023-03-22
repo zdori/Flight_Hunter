202b8edfb22854cb9250bbfb6669fd5de3d4f741
Dorottya Beata Zaszlos <dorottyabeata.zaszl@hcl.com>
2023-03-22 12:34:30 +0100

    filtering from table

--- pages/2-sms.py
+++ pages/2-sms.py
@@ -10,7 +10,7 @@ numbers_to_message = ['+36307763909', '+36307763909']
 
 def get_user_data():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select username, origin, destination, max_price from USER_PREFERENCES;")
+        my_cur.execute("select username, tier, email_noti, sms_noti, email, phone  from USER_PREFERENCES;")
         return my_cur.fetchall()
 
 def send_sms(text_msg='Hello from my Twilio number!', phone_nums=numbers_to_message):
@@ -32,8 +32,22 @@ my_cnx.close()
 
 streamlit.text(my_data_rows)
 
+send_sms = filter(lambda row: row[3], my_data_rows)
+streamlit.text(send_sms)
 
-is_clicked = streamlit.button('Send SMS')
+tier_p = filter(lambda row: row[1] == 'P', my_data_rows)
+streamlit.text(f'Premium users: {tier_p}')
+
+tier_s = filter(lambda row: row[1] == 'S', my_data_rows)
+streamlit.text(f'Standard users: {tier_s}')
+
+tier_b = filter(lambda row: row[1] == 'B', my_data_rows)
+streamlit.text(f'Basic users: {tier_b}')
+
+
+
+
+is_clicked = streamlit.button('Send Test SMS')
 
 if is_clicked:
     r = send_sms('Hello, Bob!', ['+36307763909', '+36307763909'])
